11efdd466f88e0c9ed670fd4509556d590c3e295
illegalPointer <kurzag@gmail.com>
2016-09-14 12:50:42 +0100

    Silent mode Fixed dirb

--- provision.sh
+++ provision.sh
@@ -21,18 +21,18 @@ mkdir $tools
         # Update Projects
         echo "\t\t${YELLOW}update projects${GRAY}"
         cd $utils 
-        git clone https://github.com/illegalPointer/updateprojects
+        git clone -q https://github.com/illegalPointer/updateprojects
         cp "${utils}/updateprojects/updateprojectsrecursive.sh" "${tools}/updateprojectsrecursive.sh"
 
         # Provisionsh
         echo "\t\t${YELLOW}Provisioning script${GRAY}"
         cd $utils
-        git clone https://github.com/illegalPointer/provisioningscript
+        git clone -q https://github.com/illegalPointer/provisioningscript
 
 	# Bash Template
 	echo "\t\t${YELLOW}Bash template${GRAY}"
         cd $utils
-        git clone https://github.com/illegalPointer/bashtemplate
+        git clone -q https://github.com/illegalPointer/bashtemplate
 
 # Android
 echo "${YELLOW}Android${GRAY}"
@@ -82,15 +82,15 @@ cd $infra
             # Finger user enum
             echo "${YELLOW}\t\t\tFinger-user-enum${GRAY}"
             cd $finger
-            wget http://pentestmonkey.net/tools/finger-user-enum/finger-user-enum-1.0.tar.gz
-            tar -xzvf finger-user-enum-1.0.tar.gz
+            wget -q http://pentestmonkey.net/tools/finger-user-enum/finger-user-enum-1.0.tar.gz
+            tar -xzf finger-user-enum-1.0.tar.gz
             rm finger-user-enum-1.0.tar.gz
 
         #Unix-Privesc
         echo "${YELLOW}\t\tUnix Privesc Check${GRAY}"
         cd $unix
-        wget http://pentestmonkey.net/tools/unix-privesc-check/unix-privesc-check-1.4.tar.gz
-        tar -xzvf unix-privesc-check-1.4.tar.gz
+        wget -q http://pentestmonkey.net/tools/unix-privesc-check/unix-privesc-check-1.4.tar.gz
+        tar -xzf unix-privesc-check-1.4.tar.gz
         rm unix-privesc-check-1.4.tar.gz
 
         # Bash
@@ -100,26 +100,26 @@ cd $infra
         bashversion="bash-4.3.30"
         mkdir $bash
         cd $bash
-        wget https://ftp.gnu.org/gnu/bash/${bashversion}.tar.gz
+        wget -q https://ftp.gnu.org/gnu/bash/${bashversion}.tar.gz
     
             # Bash 32
             bash32="${bash}/bash32"
             mkdir $bash32
             cd $bash
-            tar -xzvf "${bashversion}.tar.gz" -C ${bash32} --strip-components 1
+            tar -xzf "${bashversion}.tar.gz" -C ${bash32} --strip-components 1 --no-same-owner
             cd $bash32
             # Bash 32: configure32
-            ./configure --build=i686-pc-linux-gnu "CC=gcc -m32" "CXX=g++ -m32" "LDFLAGS=-m32"
+            ./configure --build=i686-pc-linux-gnu --enable-silent-rules "CC=gcc -m32" "CXX=g++ -m32" "LDFLAGS=-m32"
             make
     
             # Bash 64
             bash64="${bash}/bash64"
             mkdir $bash64
             cd $bash
-            tar -xzvf "${bashversion}.tar.gz" -C ${bash64} --strip-components 1
+            tar -xzf "${bashversion}.tar.gz" -C ${bash64} --strip-components 1 --no-same-owner
             cd $bash64
             # Bash 64: configure64
-            ./configure --build=x86_64-pc-linux-gnu "CC=gcc -m64" "CXX=g++ -m64" "LDFLAGS=-m64"
+            ./configure --build=x86_64-pc-linux-gnu --enable-silent-rules "CC=gcc -m64" "CXX=g++ -m64" "LDFLAGS=-m64"
             make
         
         cd $bash
@@ -128,8 +128,8 @@ cd $infra
     # Udp Proto scanner
     cd $infra
     echo "${YELLOW}\tUdp-proto-scanner${GRAY}"
-    wget https://labs.portcullis.co.uk/download/udp-proto-scanner-1.1.tar.gz
-    tar -xzvf udp-proto-scanner-1.1.tar.gz
+    wget -q https://labs.portcullis.co.uk/download/udp-proto-scanner-1.1.tar.gz
+    tar -xzf udp-proto-scanner-1.1.tar.gz --no-same-owner
     rm udp-proto-scanner-1.1.tar.gz
 
 # Web
@@ -140,20 +140,20 @@ mkdir $web
     # Hoppy
     echo "\t${YELLOW}Hoppy Web Scanner${GRAY}"
     cd $web
-    wget https://labs.portcullis.co.uk/download/hoppy-1.8.1.tar.bz2
-    bzip2 -d hoppy-1.8.1.tar.bz2
-    tar -xvf hoppy-1.8.1.tar
+    wget -q https://labs.portcullis.co.uk/download/hoppy-1.8.1.tar.bz2
+    bzip2 -d -q hoppy-1.8.1.tar.bz2
+    tar -xf hoppy-1.8.1.tar --no-same-owner
     rm hoppy-1.8.1.tar
 
     # Nikto
     echo "\t${YELLOW}Nikto Web Scanner${GRAY}"
     cd $web
-    git clone https://github.com/sullo/nikto
+    git clone -q https://github.com/sullo/nikto
 
     # Web Basic
     echo "\t${YELLOW}webBasic.sh${GRAY}"
     cd $web
-    git clone https://github.com/illegalPointer/webBasic.sh
+    git clone -q https://github.com/illegalPointer/webBasic.sh
     
     # Crypto
     echo "\t${YELLOW}Crypo${GRAY}"
@@ -163,18 +163,18 @@ mkdir $web
         # Test Poodle TSL        
         echo "\t\t${YELLOW}Test POODLE TLS${GRAY}"
         cd ${crypto}
-        git clone https://github.com/exploresecurity/test_poodle_tls
+        git clone -q https://github.com/exploresecurity/test_poodle_tls
 
         # Test SSL
         echo "\t\t${YELLOW}Test SSL ${GRAY}"
         cd ${crypto}
-        git clone https://github.com/drwetter/testssl.sh
+        git clone -q https://github.com/drwetter/testssl.sh
 
         # SSL Cipher Suite Enum
 	echo "\t\t${YELLOW}SSL Cipher Suite Enum${GRAY}"
         cd ${crypto}
-        wget https://labs.portcullis.co.uk/download/ssl-cipher-suite-enum-v1.0.2.tar.gz
-        tar -xzvf ssl-cipher-suite-enum-v1.0.2.tar.gz
+        wget -q https://labs.portcullis.co.uk/download/ssl-cipher-suite-enum-v1.0.2.tar.gz
+        tar -xzf ssl-cipher-suite-enum-v1.0.2.tar.gz --no-same-owner
         rm ssl-cipher-suite-enum-v1.0.2.tar.gz
 
     # Fuzzing
@@ -185,19 +185,19 @@ mkdir $web
         # Dirb
         echo "\t\t${YELLOW}Dirb${GRAY}"
         cd $fuzzing
-        wget https://sourceforge.net/projects/dirb/files/dirb/2.22/
-        tar -xzvf download
+        wget -q https://sourceforge.net/projects/dirb/files/dirb/2.22/dirb222.tar.gz/download        
+        tar -xzf download --nosameowner
         pendingtasks="${pendingtasks}\n\t- Compile Dirb, located in ${fuzzing}"
 
         # Wfuzz
         echo "\t\t${YELLOW}wFuzz${GRAY}"
         cd $fuzzing
-        git clone https://github.com/xmendez/wfuzz
+        git clone -q https://github.com/xmendez/wfuzz
 
         # wFuzz Parser
         echo "\t\t${YELLOW}wFuzz Parser${GRAY}"
         cd $fuzzing
-        git clone https://github.com/illegalPointer/wFuzzParser
+        git clone -q https://github.com/illegalPointer/wFuzzParser
     
 # Fuzzing
 echo "${YELLOW}FuzzLists${GRAY}"
@@ -207,12 +207,12 @@ mkdir $flists
     # FuzzDB
     echo "\t${YELLOW}FuzzDB${GRAY}"
     cd $flists
-    git clone https://github.com/fuzzdb-project/fuzzdb
+    git clone -q https://github.com/fuzzdb-project/fuzzdb
 
     # SecLists
     echo "\t${YELLOW}Sec Lists${GRAY}"
     cd $flists
-    git clone https://github.com/danielmiessler/SecLists
+    git clone -q https://github.com/danielmiessler/SecLists
 
 
 pendingtasks="${pendingtasks}\n\t- Add private repos, if needed"
